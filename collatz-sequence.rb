49ed7d55b9dd48ee9fceacb8bcaea81d3a388f16
Carrie <carrie.g.crow@gmail.com>
2019-11-08 00:36:38 +0000

    Initial Commit

---
+++ collatz-sequence.rb
@@ -0,0 +1,23 @@
+def collatz_sequence(number)
+  sequence = [number]
+  while number > 1
+    if number % 2 == 0
+      number = number/2
+    elsif number % 2 == 1
+      number = 3 * number + 1
+    end
+    sequence.push(number)
+  end
+  return sequence
+end
+
+def longest_collatz(highest_input)
+  i = 1
+  longest_sequence = { input: 0, length: 0 }
+  while i <= highest_input
+    sequence_length = collatz_sequence(i).length
+    longest_sequence = { input: i, length: sequence_length } if sequence_length > longest_sequence[:length]
+    i += 1
+  end
+  return longest_sequence
+end
